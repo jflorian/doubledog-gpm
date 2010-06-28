aa92615030d26d8523f15d87155c773a15ae4622
John Florian <jflorian@doubledog.org>
2010-06-27 21:41:19 -0400

    New - gpm module
    Formerly, gpm was installed as a base package.  This revision moves that
    package to a formal gpm module where a config file is also installed for
    left-handed consoles and the service is now managed.

---
+++ manifests/init.pp
@@ -0,0 +1,34 @@
+# /etc/puppet/modules/gpm/manifests/init.pp
+
+class gpm {
+
+    package { "gpm":
+	ensure	=> installed,
+    }
+
+    file { "/etc/sysconfig/mouse":
+        # don't forget to verify these!
+        group	=> "root",
+        mode    => "0640",
+        owner   => "root",
+        require => Package["gpm"],
+        seluser => "system_u",
+        selrole => "object_r",
+        seltype => "etc_t",
+        source  => "puppet:///modules/gpm/mouse",
+    }
+
+    service { "gpm":
+        enable		=> true,
+        ensure		=> running,
+        hasrestart	=> true,
+        hasstatus	=> true,
+        require		=> [
+            Package["gpm"],
+        ],
+        subscribe	=> [
+            File["/etc/sysconfig/mouse"],
+        ],
+    }
+
+}
