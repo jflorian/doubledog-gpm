b06059ff9c5622de091c5cd771fb1cb899b3e96c
John Florian <jflorian@doubledog.org>
2015-10-31 22:03:46 -0400

    Janitorial - puppet-lint compliance
    This is the result of running 'puppet-lint --fix modules/'.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,32 +2,32 @@
 
 class gpm {
 
-    package { "gpm":
-	ensure	=> installed,
+    package { 'gpm':
+  ensure  => installed,
     }
 
-    file { "/etc/sysconfig/mouse":
+    file { '/etc/sysconfig/mouse':
         # don't forget to verify these!
-        group	=> "root",
-        mode    => "0640",
-        owner   => "root",
-        require => Package["gpm"],
-        seluser => "system_u",
-        selrole => "object_r",
-        seltype => "etc_t",
-        source  => "puppet:///modules/gpm/mouse",
+        group   => 'root',
+        mode    => '0640',
+        owner   => 'root',
+        require => Package['gpm'],
+        seluser => 'system_u',
+        selrole => 'object_r',
+        seltype => 'etc_t',
+        source  => 'puppet:///modules/gpm/mouse',
     }
 
-    service { "gpm":
-        enable		=> true,
-        ensure		=> running,
-        hasrestart	=> true,
-        hasstatus	=> true,
-        require		=> [
-            Package["gpm"],
+    service { 'gpm':
+        enable     => true,
+        ensure     => running,
+        hasrestart => true,
+        hasstatus  => true,
+        require    => [
+            Package['gpm'],
         ],
-        subscribe	=> [
-            File["/etc/sysconfig/mouse"],
+        subscribe  => [
+            File['/etc/sysconfig/mouse'],
         ],
     }
 
